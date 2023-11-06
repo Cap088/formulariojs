1e068ac97f4406bfdcb7ac65b2963e1fe541865c
Cap088 <141892017+Cap088@users.noreply.github.com>
2023-11-06 16:44:05 -0500

    Se agrega el Formulario

---
+++ index.js
@@ -0,0 +1,41 @@
+
+var saludo = "Hola Bienvenido al Formulario";
+    alert(saludo);
+
+//funcion
+function mostrarInputs(){
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let masculino = document.getElementById("masculino").checked;
+    let femenino = document.getElementById("femenino").checked;
+    let tyc = document.getElementById('tyc').checked;
+    let resultado = masculino + femenino;
+    
+    
+    //condicionales de ingreso de datos
+    if(nombre == "" ){
+        alert("Por favor ingresa el nombre...")
+    }
+    if(nombre == "" ){
+        alert("Por favor ingresa el apellido...")
+    }
+    
+
+   if(resultado == 0){
+        alert("Por favor seleciona el sexo...")
+   }
+
+    if(tyc == false){
+        alert("Por favor acepta los terminos...")
+    }
+
+     //visualizador de ingresos
+     console.log(nombre);
+     console.log(apellido);
+     console.log(resultado);
+     console.log(tyc);
+
+    return false;
+
+
+}
